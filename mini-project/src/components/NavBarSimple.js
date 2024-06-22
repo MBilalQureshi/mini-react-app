78bbf481e92a3c652442eef30e422f16101ae2de
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-06-22 13:30:07 +0000

    Add simple navbar using stateful class component concept

---
+++ mini-project/src/components/NavBarSimple.js
@@ -0,0 +1,31 @@
+import React,{Component} from "react";
+import css from "./css/NavBarSimple.module.css"
+
+export default class NavBarSimple extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            message: 'Hello, guest!',
+            buttonText: 'Login',
+        }
+    }
+    handleClick = () => {
+        this.setState((prevState, prevProps)=>{
+            return{
+                message: prevState.message === 'Hello, guest!' ? 'Welcome back, user!' : 'Hello, guest!',
+                buttonText: prevState.buttonText === 'Login' ? 'Logout' : 'Login',
+            }
+        })
+    }
+    render(){
+        return(
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={()=>this.handleClick()}>{this.state.buttonText}</button>
+                </div>
+            </div>
+        )
+    }
+}
